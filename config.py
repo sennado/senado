4058a9add4ea71c569bdb78e862544357e8313f0
sennado <wcsne@foxmail.com>
2018-03-07 16:08:40 +0800

    提交配置文件

---
+++ config.py
@@ -0,0 +1,13 @@
+# !/usr/bin/evn python
+# -*- coding:utf-8 -*-
+
+__auther__ = 'sennado'
+
+
+mysqlDb = dict(
+    host = '127.0.0.1',
+    port = '3306',
+    user = 'root',
+    password = 'root',
+    dbname = 'name',
+)
--- run.py
+++ run.py
@@ -3,6 +3,14 @@
 
 __auther__ = 'sennado'
 
+import sys  
+if sys.prefix[-2:] is not '35':
+    print(sys.prefix[-2:] == '35') 
+
 from tornado import web, ioloop, gen, options
 
-print(1111)
+from config import *
+
+ 
+
+print(mysqlDb)
