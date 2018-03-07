fedea054e612028071a1e84da51da1ba1e91270d
sennado <wcsne@foxmail.com>
2018-03-07 16:39:58 +0800

    入口测试

--- run.py
+++ run.py
@@ -4,13 +4,33 @@
 __auther__ = 'sennado'
 
 import sys  
-if sys.prefix[-2:] is not '35':
-    print(sys.prefix[-2:] == '35') 
 
-from tornado import web, ioloop, gen, options
+from tornado import web, ioloop, gen
 
+from tornado.options import define, options
 from config import *
 
- 
 
+define("port", default=8999, help="run port", type=int)
+
+print(options.port)
 print(mysqlDb)
+settings = {
+    'debug' : True
+}
+
+class MainHandler(web.RequestHandler):
+    ''' docstring for MainHandler '''
+    def get(self):
+        
+        self.write("Hello word!")
+
+def main():
+    return web.Application([
+        (r"/", MainHandler),
+    ], **settings)
+
+if __name__ == "__main__":
+    app = main()
+    app.listen(options.port)
+    ioloop.IOLoop.instance().start()   
